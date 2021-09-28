eddeb95857949017b1d60edeae642e44f093363a
Hathor Hamilton <koshknot@gmail.com>
2021-09-28 16:25:40 -0400

    Weird Bug Mode

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import CakeForm from "./CakeForm";
 import FlavorZone from './FlavorZone'
 
 function App() {
+  // states
   const [visible, setVisible] = useState(false);
   const [selectedCake , setSelectedCake] = useState(null)
   const [cakeList, setCakes] = useState([])
   const [flavorSaver , setFlavorSaver] = useState([])
 
+ //effects
   useEffect(()=> {
     fetch('http://localhost:4000/cakes')
     .then( resp => resp.json())
@@ -24,12 +26,59 @@ function App() {
   )
 
   
+  // functions
+
+  function handleUpdate(cake){
+    console.log(cake)
+    fetch(`http://localhost:4000/cakes/${cake.id}`,{
+      method:'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({liked:!cake.liked})
+    })
+    .then(res => res.json())
+    .then(updatedCake => {
+      const updatedCakeList = cakeList.map(clCake => clCake.id === cake.id? updatedCake : clCake);
+      
+      
+    setSelectedCake(updatedCake)
+    setCakes(updatedCakeList)
+    });
+  }
+
+
 
   function handleAddCake(cake){
-    setCakes([
-      ...cakeList, cake
-    ])
+    fetch('http://localhost:4000/cakes',{
+      method : 'POST',
+      headers: {
+        "COntent-Type": 'application/json',
+      },
+      body: JSON.stringify(cake)
+    })
+    .then(res => res.json())
+    .then(newCake => {
+      setCakes([
+      ...cakeList, newCake
+      ])
+  })
+}  
+    
+  function handleDelete(id){
+      fetch(`http://localhost:4000/cakes/${id}`,{
+        method : 'DELETE'
+      })
+      .then(res => res.json())
+      .then(() => {
+        const filteredCakes = cakeList.filter(cake => cake.id !== id)     
+        setCakes(filteredCakes)
+        setSelectedCake(null)
+      }
+    )
   }
+
+
 
   return ( 
     <>
@@ -40,7 +89,7 @@ function App() {
     <br/>
     <FlavorZone flavorSaver = {flavorSaver}/>
     <br></br>
-    {selectedCake? <CakeDetail cake={selectedCake} /> : null}
+    {selectedCake? <CakeDetail handleUpdate={handleUpdate} handleDelete={handleDelete} cake={selectedCake} /> : null}
     {cakeList.map(cake => <CakeCard cake={cake} setSelectedCake={setSelectedCake}/> )}
   </>
   );
--- src/CakeDetail.jsx
+++ src/CakeDetail.jsx
@@ -1,5 +1,5 @@
 
-function CakeDetail({cake}) {
+function CakeDetail({cake , handleDelete, handleUpdate}) {
     return (
         <>
         <img src={cake.image} style={{width:"200px"}}/>
@@ -7,6 +7,8 @@ function CakeDetail({cake}) {
         <p>{cake.size}</p>
         <p>${cake.price}</p>
         <p>{cake.description}</p>
+        <button onClick={()=>handleUpdate(cake)}>{cake.liked?"❤️" : "♡"}</button>
+        <button onClick={()=>handleDelete(cake.id)}>Remove Cake</button>
         </>
     );
 }
